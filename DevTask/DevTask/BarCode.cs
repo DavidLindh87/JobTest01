af26a1cfa68726466f520ee6912a52e1f86147c4
DavidLindh87 <70029517+DavidLindh87@users.noreply.github.com>
2020-08-24 16:47:22 +0200

    Added all of the files for this task. The program works as intended on my computer please contact me if there is any issues. I used VS 2019 the free version.

---
+++ DevTask/DevTask/BarCode.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace DevTask
+{
+    class BarCode
+    {
+        //private string Left_Guard;
+        //private string Center_Guard;
+        //private string Right_Guard;
+        private string new_lines;
+
+
+        private void Barcode()
+        {
+            //we initialize the variables in the constructor
+            new_lines = string.Empty;
+        }
+
+        public List<String> Convert_To_Numbers(string[] input)
+        {
+            List<String> my_input = new List<String>();
+            //we loop through each character in each string in the array and convert it into either ones or zeros
+            foreach (string line in input)
+            {
+                foreach (char c in line)
+                {
+                    if(Char.IsWhiteSpace(c))
+                    {
+                        new_lines += "0";
+                    }
+                    else
+                    {
+                        new_lines += "1";
+                    }
+                }
+                //Now that we have the line of ones and zeros we can sift out the Left-, Center- and Right Guards 
+                //and separate the numbers in the remaining string, but lets do it in another function to keep it seperate
+                new_lines = Binary_To_Integer(new_lines);
+                my_input.Add(new_lines);
+                new_lines=string.Empty;
+            }
+            return my_input;
+        }
+
+        public string Binary_To_Integer(string barcode)
+        {
+            string converted_string = string.Empty;
+            string unguarded_string = barcode;
+
+            //first we remove and store the guards from the binary barcode
+            unguarded_string = unguarded_string.Remove(0, 3); //remove Left Guard
+            unguarded_string = unguarded_string.Remove(42, 5); //remove Middle Guard
+            unguarded_string = unguarded_string.Remove(84); //remove Right Guard
+
+            //now we want to split the remaining string into substrings of size 7
+            int size = 7;
+            IEnumerable<string> split_string = unguarded_string.Split(size);
+            //now we need to decode the barcode 
+            converted_string = Decoding(split_string);
+            //string test = String.Join(Environment.NewLine, split_string);           
+            return converted_string;
+        }
+
+        private string Decoding(IEnumerable<string> spt_str)
+        {
+            IEnumerator<string> e = spt_str.GetEnumerator();
+            List<string> converted = new List<string>();
+            string final_str = string.Empty;
+            while (e.MoveNext())
+            {
+                string tmp = e.Current;
+                string conversion = string.Empty;
+                int n = 1;
+                for(int i = 0; i < tmp.Length; i++){                    
+                    if ((i + 1)!=tmp.Length)
+                    {
+                        if (tmp[i].Equals(tmp[i + 1]))
+                        {
+                            n++;
+                        }
+                        else
+                        {
+                            conversion += n.ToString();
+                            n = 1;
+                        }
+                    }
+                    else
+                    {
+                        conversion += n.ToString();
+                    }
+                }
+                converted.Add(conversion);
+            }
+            //now we use the numbers in the converted list to compare with the preexisting comparisons to numbers for Left and Right side and return it
+            final_str = Final_Conversion(converted);
+            return final_str;
+            //return converted[0] + converted[1] + converted[2] + converted[3] + converted[4] + converted[5] + converted[6];
+        }
+
+        private string Final_Conversion(List<string> lst_str)
+        {
+            var output = new StringBuilder();
+            for (int i = 0; i < lst_str.Count; i++)
+            {
+                if(lst_str[i].Equals("3211"))
+                {
+                    output.Append("0");
+                }else if (lst_str[i].Equals("2221"))
+                {
+                    output.Append("1");
+                }
+                else if (lst_str[i].Equals("2122"))
+                {
+                    output.Append("2");
+                }
+                else if (lst_str[i].Equals("1411"))
+                {
+                    output.Append("3");
+                }
+                else if (lst_str[i].Equals("1132"))
+                {
+                    output.Append("4");
+                }
+                else if (lst_str[i].Equals("1231"))
+                {
+                    output.Append("5");
+                }
+                else if (lst_str[i].Equals("1114"))
+                {
+                    output.Append("6");
+                }
+                else if (lst_str[i].Equals("1312"))
+                {
+                    output.Append("7");
+                }
+                else if (lst_str[i].Equals("1213"))
+                {
+                    output.Append("8");
+                }
+                else if (lst_str[i].Equals("3112"))
+                {
+                    output.Append("9");
+                }
+            }
+            //Console.WriteLine(output.ToString());
+            return output.ToString();
+        }
+    }
+}
---
+++ DevTask/DevTask/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DevTask
+{
+    class Program
+    {
+
+        private string[] ReadFromFile(String in_path)
+        {
+            return File.ReadAllLines(in_path);
+        }
+
+        private void WriteToFile(String[] result, String out_path)
+        {
+            File.WriteAllLines(out_path, result);
+        }
+
+        private static string Set_IO_Path()
+        {
+            string currentDir = Environment.CurrentDirectory;
+            //might have to change the number of  "..\" here depending on from where you run the program
+            DirectoryInfo directory = new DirectoryInfo(Path.GetFullPath(Path.Combine(currentDir, @"..\..\..\..\..\")));
+            return directory.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            //we begin by getting the path to the parent directory of where the input file is located via the Set_IO_Path function
+            string base_path = Set_IO_Path();
+
+            //we then add the specific sections leading to the desired input file and the designated outputfile
+            string in_path = base_path + @"WinDevTaskA\testdata1.txt";
+            string out_path = base_path + @"WinDevTaskA\outputdata.txt";
+
+            List<String> my_output = new List<String>();
+            //here we read all lines in the input file and store them in an array of strings
+            string[] read_text = File.ReadAllLines(in_path);
+
+            //here we do all the conversion of the input before putting it in output
+            //first we initiate an object of the BarCode class type
+            BarCode bar = new BarCode();
+            //this function of the barcode class converts the barcode into numbers and stores them in a list of strings
+            my_output = bar.Convert_To_Numbers(read_text);
+
+            //Here we print the output
+            File.WriteAllLines(out_path, my_output);
+
+        }
+    }
+}
